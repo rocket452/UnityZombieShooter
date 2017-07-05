ed4645d9b046c22a19ccf1fab2b2e6da69f0ab24
rocket452 <bradygilley45@gmail.com>
2017-07-05 01:00:21 -0400

    d

---
+++ ZombieShooterProject/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform playerSpawnPoints;
+    private Transform[] spawnPointsArray;
+    public bool respawnBool = false;
+    private bool lastToggle = false;
+
+	// Use this for initialization
+	void Start () {
+        spawnPointsArray = playerSpawnPoints.GetComponentsInChildren<Transform>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(lastToggle != respawnBool)
+        {
+            Respawn();
+            respawnBool = false;
+        }
+        else
+        {
+            lastToggle = respawnBool;
+        }
+		
+	}
+
+    private void Respawn()
+    {
+        int i = Random.Range(1, spawnPointsArray.Length);
+        transform.position = spawnPointsArray[i].transform.position;
+
+    }
+}
