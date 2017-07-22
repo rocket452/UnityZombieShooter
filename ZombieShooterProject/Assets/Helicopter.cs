a87e835f534c996fda546dffab5457e7bbcfec7b
rocket452 <bradygilley45@gmail.com>
2017-07-21 23:38:49 -0400

    last commit

---
+++ ZombieShooterProject/Assets/Helicopter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    private bool heliCalled = false;
+   // public AudioClip callSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("CallHeli") && !heliCalled)
+        {
+            heliCalled = true;
+            print("Heli Called");
+        }
+    
+
+    }
+}
