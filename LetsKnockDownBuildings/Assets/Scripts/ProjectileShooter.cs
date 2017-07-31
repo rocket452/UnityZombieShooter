d070be918371924ae16e7eeeb812d7dcd004b53e
rocket452 <bradygilley45@gmail.com>
2017-07-31 00:39:52 -0400

    d

--- LetsKnockDownBuildings/Assets/Scripts/ProjectileShooter.cs
+++ LetsKnockDownBuildings/Assets/Scripts/ProjectileShooter.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class ProjectileShooter : MonoBehaviour {
 
+    GameObject prefab;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        prefab = Resources.Load("projectile") as GameObject;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward * 40;
+        }
 		
 	}
 }
